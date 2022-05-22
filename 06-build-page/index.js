169e970ff4c3df56490b70783818d6c044c19d2d
Val Sotnik <cmvaltime@gmail.com>
2022-05-22 13:48:24 +0300

    feat:copy assets function add to 06-build-page, solition complete

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,12 +2,10 @@ const fs = require('fs');
 const fsPromises = require('fs/promises');
 const path = require('path');
 
-
-
-// ====Make project-dist directory and index.html======================
+// ====Make project-dist directory and put in filled index.html file======================
 const templatePath = path.join(__dirname, 'template.html');
 const componentsPath = path.join(__dirname, 'components');
-const finalProjectPath = path.join(__dirname, 'project-dist');
+const projectDistPath = path.join(__dirname, 'project-dist');
 
 async function buildHTML() {
   const html = await fsPromises.readFile(templatePath);
@@ -23,11 +21,11 @@ async function buildHTML() {
       innerPageContent = innerPageContent.replace(`{{${componentName}}}`, componentContent.toString());
     }
   }
-  await fsPromises.mkdir(finalProjectPath, { recursive: true });
-  await fsPromises.writeFile(path.join(finalProjectPath, 'index.html'), innerPageContent);
+  await fsPromises.mkdir(projectDistPath, { recursive: true });
+  await fsPromises.writeFile(path.join(projectDistPath, 'index.html'), innerPageContent);
 }
 
-//======= Make bundle styles from origin css===========
+//======= Make bundle styles from origin css files ================
 const stylesDir = path.join(__dirname, 'styles');
 
 async function stylesBundle () {
@@ -44,44 +42,34 @@ async function stylesBundle () {
   }
 }
 
-
-// ===== Copy assets dir===================
+// ===== Copy assets===========================================
 const originAssets = path.join(__dirname,  'assets');
 const duplicateAssets = path.join(__dirname, 'project-dist' ,'assets');
 
-async function copyAssetsDir (inputPath, outputPath) {
+async function copyAssetsDir (inputDir, outputDir) {
 
-  await fsPromises.mkdir(outputPath, { recursive: true });
-  let dirFiles =  await fsPromises.readdir(outputPath);
+  await fsPromises.mkdir(duplicateAssets, { recursive: true });
+  const dirFiles = await fsPromises.readdir(inputDir, {withFileTypes: true});
   
-  for (let file of dirFiles) {
-    await fsPromises.rm(path.join(outputPath, file));
-  }
-
-  dirFiles = await fsPromises.readdir(inputPath);
-  
-  for (let file of dirFiles) {
-
-    let stats = await fsPromises.stat(path.join(dirFiles, file));
-    if (!stats.isDirectory()) {
-      await fsPromises.copyFile(path.join(inputPath, file), path.join(outputPath, file));
-    } else {
-      await copyAssetsDir(path.join(inputPath, file), path.join(outputPath, file));
+  dirFiles.forEach(async (file) => {
+    if(file.isFile()) {
+      fsPromises.copyFile(path.join(inputDir, file.name), path.join(outputDir, file.name));
+    } else if (file.isDirectory()) {
+      await fsPromises.mkdir(path.join(outputDir, file.name));
+      await copyAssetsDir(path.join(inputDir, file.name), path.join(outputDir, file.name));
     }
-  }
-
+  });
 
 }
 
 
+//=======Construct of output project==================================
 
-//=======Make output project============
-
-(async function builProject() {
+(async () => {
 
   await buildHTML();
   await stylesBundle();
-  // await copyAssetsDir(originAssets, duplicateAssets);
+  await copyAssetsDir(originAssets, duplicateAssets);
 })();
 
 
